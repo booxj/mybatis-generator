ee068821016c57e08d50cb8903cb9791472f5c8c
汪博 <wb@wbdeMac-mini.local>
2017-02-07 14:57:38 +0800

    fix

---
+++ src/main/java/com/boplus/mapper/TUserMapper.java
@@ -0,0 +1,17 @@
+package com.boplus.mapper;
+
+import com.boplus.pojo.TUser;
+
+public interface TUserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(TUser record);
+
+    int insertSelective(TUser record);
+
+    TUser selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+}
